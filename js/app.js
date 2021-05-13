d79d37506897e6b5432e4897f9916d9368cc98f2
x <bmariusz@vp.pl>
2021-05-13 20:32:18 +0200

    class producto

--- js/app.js
+++ js/app.js
@@ -54,33 +54,67 @@ function producto(posX, posY, id, AttributProducto, msg = '', cls = 'articulo', 
 
 // declaracion de atributos para class = articulo
 let ArticuloStyle = [
-    '200px',        //width
-    '200px',        //height
-    'green',        //background: bg-color bg-image position/bg-size bg-repeat bg-origin bg-clip bg-attachment initial|inherit;
+    '300px',        //width
+    '300px',        //height
+    'white',        //background: bg-color bg-image position/bg-size bg-repeat bg-origin bg-clip bg-attachment initial|inherit;
     'left',         //text_align
     '0px' ,         //padding
     'normal normal 400 16px Arial, Helvetica, sans-serif', //font
     'black',        //color
     '0px',          //margin
-    '3px solid red' //border
+    '0px solid white' //border
 ];
+
 // creacion de objeto BoxStyleX para clase articulo
-let x1 = new BoxStyleX( ...ArticuloStyle);
+class BoxPaginaPrincipal {
+    constructor( id, imag, descripcion, precio, posicionX, posicionY){
+        this.id = id;
+        this.imag = imag;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.posicionX = posicionX;
+        this.posicionY = posicionY;
+        this.crea();
+ 
+    }
 
-// creacion de objeto BoxStyleX para clase imagen cambiando objeto creado para articulo
-let ImagenStyle = ArticuloStyle;
-ImagenStyle[0] = '80px';
-ImagenStyle[1] = '80px';
-ImagenStyle[2] = `lightblue url("../img/fondo.jpg") no-repeat fixed center`
-ImagenStyle[8] = `3px solid blue`
-let x2 = new BoxStyleX( ...ImagenStyle);
+    createSubBox(){
+        let x1 = new BoxStyleX( ...ArticuloStyle);
+        producto(this.posicionX, this.posicionY, `${this.id}`, x1, '');
+    }
+    createImageBox(){
+        let ImagenStyle = ArticuloStyle;
+        ImagenStyle[0] = '100%';
+        ImagenStyle[1] = '67%';
+        ImagenStyle[2] = `no-repeat url("${this.imag}")`
+        let x2 = new BoxStyleX( ...ImagenStyle);
+        producto (0,0, `${this.id}_img`, x2,  '', 'image', `${this.id}`);
+    }
+    createDescription(){
+        let ImagenStyle = ArticuloStyle;
+        ImagenStyle[0] = '200';
+        ImagenStyle[1] = '80';
+        ImagenStyle[2] = `white`
+        let x3 = new BoxStyleX( ...ImagenStyle);
+        producto ('25','220', `${this.id}_msg`, x3,`${this.descripcion}`, 'msg', `${this.id}`);
+    }
+    createPrecio(){
+        let ImagenStyle = ArticuloStyle;
+        ImagenStyle[0] = '90px';
+        ImagenStyle[1] = '50px';
+        ImagenStyle[2] = `white`
+        let x4 = new BoxStyleX( ...ImagenStyle);
+        producto ('200','250', `${this.id}_precio`, x4, `${this.precio}`, 'precio', `${this.id}`);
+    }
+    async crea(){
+        await this.createSubBox();
+        this.createImageBox();
+        this.createDescription();
+        this.createPrecio();
+    }
+}
 
-producto(100, 100, id = 'prod1', x1, '<p>text</p>');
-
-let txt1 = `<h1>Visita la Estación Espacial</h1> 
-             <p>Más de 200 personas han visitado la Estación Espacial Internacional</p>`
-  
-producto (20,20, 'subpr1', x2, msg = txt1, cls = 'image', padreId = 'prod1');
+let box1 = new BoxPaginaPrincipal('nuevo', 'img/tierra.jpg', '<h3>titel</h3><p>dlafjlsdfjlasl</p>', '50 000', 400, 400)
 
 
 
